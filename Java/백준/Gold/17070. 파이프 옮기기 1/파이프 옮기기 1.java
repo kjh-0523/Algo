7cb359b849d97a57106f74006b1019e6b04db4ce
kjh-0523 <124140035+kjh-0523@users.noreply.github.com>
2024-08-26 15:11:30 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 288 ms, Memory: 15000 KB -BaekjoonHub

--- Java/백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ Java/백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -1,13 +1,10 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
 import java.util.StringTokenizer;
-/*시간 : 760ms 메모리 : 397384kb*/
+
 public class Main {
 	static int[] dx = {0,1,1};
 	static int[] dy = {1,1,0};
-	
 	static int[][] map;
 	static int N;
 	public static void main(String[] args) throws Exception{
@@ -17,33 +14,25 @@ public class Main {
 		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine()," ");
 			for (int j = 0; j < N; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
+				map[i][j] = -Integer.parseInt(st.nextToken());
 			}
 		}
-		
-		Deque<int[]> queue = new ArrayDeque<>();
-		queue.offer(new int[] {0,1,0});
-		int nx,ny,x,y,s;
-		int ans = 0;
-		while(!queue.isEmpty()) {
-			int[] now = queue.poll();
-			x = now[0];
-			y = now[1];
-			s = now[2];
-			for (int i = 0; i < 3; i++) {
-				if(Math.abs(s - i)>=2) continue;
-				nx = x + dx[i];
-				ny = y + dy[i];
-				if(nx > -1 && nx < N && ny > -1 && ny < N && map[nx][ny] != 1) {
-					if(i == 1 && (map[nx-1][ny] == 1 || map[nx][ny-1] == 1)) continue;
-					if(nx == N-1 && ny == N-1) {
-						ans++;
-						continue;
-					}
-					queue.offer(new int[] {nx,ny,i});
-				}
-			}
-		}
+		dfs(0,1,0);
+		int ans= 0;
+		if(map[N-1][N-1] == -1) ans = 0;
+		else ans = map[N-1][N-1];
 		System.out.println(ans);
+	}
+	public static void dfs(int x, int y, int d) {
+		int nx, ny;
+		for (int i = 0; i < 3; i++) {
+			if(Math.abs(d - i) >= 2) continue;
+			nx = x + dx[i];
+			ny = y + dy[i];
+			if(nx > N-1 || ny > N-1 || map[nx][ny] == -1) continue;
+			if(i == 1 && (map[nx-1][ny] == -1 || map[nx][ny-1] == -1))continue;
+			map[nx][ny]++;
+			dfs(nx,ny,i);
+		}
 	}
 }
