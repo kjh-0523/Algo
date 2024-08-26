c1f922db1af2fe4f7b5673010e7ff0e3791a6c38
kjh-0523 <124140035+kjh-0523@users.noreply.github.com>
2024-08-26 15:37:36 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 104 ms, Memory: 14264 KB -BaekjoonHub

--- Java/백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ Java/백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -3,36 +3,37 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int[] dx = {0,1,1};
-	static int[] dy = {1,1,0};
 	static int[][] map;
 	static int N;
-	public static void main(String[] args) throws Exception{
+	static int[][][] dp = new int[20][20][3];
+
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		map = new int[N][N];
-		for (int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine()," ");
-			for (int j = 0; j < N; j++) {
-				map[i][j] = -Integer.parseInt(st.nextToken());
+		map = new int[N + 1][N + 1];
+		for (int i = 1; i <= N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			for (int j = 1; j <= N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		dfs(0,1,0);
-		int ans= 0;
-		if(map[N-1][N-1] == -1) ans = 0;
-		else ans = map[N-1][N-1];
+		dp[1][2][0] = 1;
+		int ans = dp();
 		System.out.println(ans);
 	}
-	public static void dfs(int x, int y, int d) {
-		int nx, ny;
-		for (int i = 0; i < 3; i++) {
-			if(Math.abs(d - i) >= 2) continue;
-			nx = x + dx[i];
-			ny = y + dy[i];
-			if(nx > N-1 || ny > N-1 || map[nx][ny] == -1) continue;
-			if(i == 1 && (map[nx-1][ny] == -1 || map[nx][ny-1] == -1))continue;
-			map[nx][ny]++;
-			dfs(nx,ny,i);
+
+	public static int dp() {
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				if (map[i][j] == 1) 
+					continue;
+				dp[i][j][0] += dp[i][j - 1][0] + dp[i][j - 1][2];
+				dp[i][j][1] += dp[i - 1][j][1] + dp[i - 1][j][2];
+				if (map[i - 1][j] == 0 && map[i][j - 1] == 0) {
+					dp[i][j][2] += dp[i - 1][j - 1][0] + dp[i - 1][j - 1][1] + dp[i - 1][j - 1][2];
+				}
+			}
 		}
+		return dp[N][N][0] + dp[N][N][1] + dp[N][N][2];
 	}
 }
