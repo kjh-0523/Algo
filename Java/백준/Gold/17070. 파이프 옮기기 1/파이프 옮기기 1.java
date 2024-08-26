d79afc8baa80a30033622bb48c8eae4de2d9832a
kjh-0523 <124140035+kjh-0523@users.noreply.github.com>
2024-08-26 14:36:59 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 760 ms, Memory: 397284 KB -BaekjoonHub

---
+++ Java/백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int[][] dx = {{0,1,0},{0,1,1},{0,1,1}};
+	static int[][] dy = {{1,1,0},{1,1,0},{0,1,0}};
+	
+	static int[][] map;
+	static int N;
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		map = new int[N][N];
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine()," ");
+			for (int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		Deque<int[]> queue = new ArrayDeque<>();
+		queue.offer(new int[] {0,1,0});
+		int nx,ny,x,y,s;
+		int ans = 0;
+		while(!queue.isEmpty()) {
+			int[] now = queue.poll();
+			x = now[0];
+			y = now[1];
+			s = now[2];
+			for (int i = 0; i < 3; i++) {
+				nx = x + dx[s][i];
+				ny = y + dy[s][i];
+				if((nx == x && ny == y)) continue;
+				if(nx > -1 && nx < N && ny > -1 && ny < N && map[nx][ny] != 1) {
+					if(i == 1 && (map[nx-1][ny] == 1 || map[nx][ny-1] == 1)) continue;
+					if(nx == N-1 && ny == N-1) {
+						ans++;
+						continue;
+					}
+					queue.offer(new int[] {nx,ny,i});
+				}
+			}
+		}
+		System.out.println(ans);
+	}
+}
