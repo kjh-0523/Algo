702df9b44752048f165c79b83fc0cf9bc9c5674c
kjh-0523 <124140035+kjh-0523@users.noreply.github.com>
2024-09-03 22:44:49 +0900

    [Gold IV] Title: 최단경로, Time: 764 ms, Memory: 104992 KB -BaekjoonHub

--- Java/백준/Gold/1753. 최단경로/최단경로.java
+++ Java/백준/Gold/1753. 최단경로/최단경로.java
@@ -1,76 +1,68 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static class Node {
+	static class Node{
 		int vertex, weight;
 		Node next;
-
 		public Node(int vertex, int weight, Node next) {
 			this.vertex = vertex;
 			this.weight = weight;
 			this.next = next;
 		}
 	}
-	static int[] minDistance;
-	public static void main(String[] args) throws Exception {
+	static int[] distance;
+	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		
 		int V = Integer.parseInt(st.nextToken());
 		int E = Integer.parseInt(st.nextToken());
-		st = new StringTokenizer(br.readLine(), " ");
-		int start = Integer.parseInt(st.nextToken()) - 1;
+		st = new StringTokenizer(br.readLine()," ");
+		int start = Integer.parseInt(st.nextToken())-1;
 //		int end = Integer.parseInt(st.nextToken());
-
+		
 		Node[] adjList = new Node[V];
 		for (int i = 0; i < E; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			int from = Integer.parseInt(st.nextToken()) - 1;
-			int to = Integer.parseInt(st.nextToken()) - 1;
+			st = new StringTokenizer(br.readLine()," ");
+			int from = Integer.parseInt(st.nextToken())-1;
+			int to = Integer.parseInt(st.nextToken())-1;
 			int weight = Integer.parseInt(st.nextToken());
 			adjList[from] = new Node(to, weight, adjList[from]);
 		}
 		Dijkstra(adjList, start);
 		for (int i = 0; i < V; i++) {
-			if (minDistance[i] == Integer.MAX_VALUE)
-				System.out.println("INF");
-			else
-				System.out.println(minDistance[i]);
+			if(distance[i] == Integer.MAX_VALUE) System.out.println("INF");
+			else System.out.println(distance[i]);
 		}
-
+		
 	}
-
+	
 	static int Dijkstra(Node[] adjList, int start) {
-		final int V = adjList.length;
-		minDistance = new int[V];
-		boolean[] visited = new boolean[V];
-		final int INF = Integer.MAX_VALUE;
-
-		Arrays.fill(minDistance, INF);
-		minDistance[start] = 0;
-
-		for (int i = 0; i < V; i++) {
-			// step 1 : 미방문 정점 중 시작점에서 가장 가까운 정점 선택
-			int min = INF;
-			int stopOver = -1;
-			for (int j = 0; j < V; j++) {
-				if (!visited[j] && minDistance[j] < min) {
-					min = minDistance[j];
-					stopOver = j;
-				}
-			}
-//			if(stopOver == end) break;
-			if (stopOver == -1)
-				break;
-			visited[stopOver] = true;
-
-			// step2 : 선택된 정점을 경유해서 미방문 인접한 정점으로의 최소비용을 갱싱할 수 있는지 체크
-			for (Node next = adjList[stopOver]; next != null; next = next.next) {
-				if (!visited[next.vertex] && minDistance[next.vertex] > min + next.weight) {
-					minDistance[next.vertex] = min + next.weight;
+		int V = adjList.length;
+		int INF = Integer.MAX_VALUE;
+		PriorityQueue<int[]> q = new PriorityQueue<>((o1,o2)-> o1[1] - o2[1]);
+		distance = new int[V];
+		Arrays.fill(distance, INF);
+		
+		distance[start] = 0;
+		q.offer(new int[] {start,distance[start]});
+		
+		while(!q.isEmpty()) {
+			int[] stopOver = q.poll();
+			int cur = stopOver[0];
+			int dis = stopOver[1];
+			
+//			if(cur == end) break;
+			if(distance[cur] < dis) continue;
+			
+			for (Node next = adjList[cur]; next != null; next = next.next) {
+				if(distance[next.vertex] > dis + next.weight) {
+					distance[next.vertex] = dis + next.weight;
+					q.offer(new int[] {next.vertex, distance[next.vertex]});
 				}
 			}
 		}
