be19216c2af5d5564165d761326d85500355dd38
kjh-0523 <124140035+kjh-0523@users.noreply.github.com>
2024-09-17 21:09:16 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 644 ms, Memory: 127264 KB -BaekjoonHub

--- Java/백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ Java/백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
-/*시간 : 2172ms 메모리 : 141424kb*/
+/*시간 : 1568ms 메모리 : 135396kb*/
 public class Main {
 
 	public static void main(String[] args) throws Exception {
@@ -10,7 +10,7 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine()," ");
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-		
+		StringBuilder sb = new StringBuilder();
 		int[][] sum = new int[N+1][N+1];
 		for (int i = 1; i < N+1; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -25,7 +25,9 @@ public class Main {
 			int y1 = Integer.parseInt(st.nextToken());
 			int x2 = Integer.parseInt(st.nextToken());
 			int y2 = Integer.parseInt(st.nextToken());
-			System.out.println(sum[x2][y2] - sum[x1-1][y2] - sum[x2][y1-1] + sum[x1-1][y1-1]);
+			int ans = sum[x2][y2] - sum[x1-1][y2] - sum[x2][y1-1] + sum[x1-1][y1-1];
+			sb.append(ans).append("\n");
 		}
+		System.out.println(sb);
 	}
 }
