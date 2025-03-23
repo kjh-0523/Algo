4567bf9cfcd346ddea7fb29062009b19bd0d22ad
kjh-0523 <124140035+kjh-0523@users.noreply.github.com>
2025-03-23 11:26:18 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 19.38 ms, Memory: 134 MB -BaekjoonHub

---
+++ C/프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// sequence_len은 배열 sequence의 길이입니다.
+long long solution(int sequence[], size_t sequence_len) {
+    long long answer = 0;
+    return answer;
+}
