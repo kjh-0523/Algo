07fc1c9dc64b99e4881e38c422b6eac637bee071
kjh-0523 <124140035+kjh-0523@users.noreply.github.com>
2025-03-23 11:44:17 +0900

    [level 2] Title: 요격 시스템, Time: 263.99 ms, Memory: 189 MB -BaekjoonHub

---
+++ C/프로그래머스/2/181188. 요격 시스템/요격 시스템.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// targets_rows는 2차원 배열 targets의 행 길이, targets_cols는 2차원 배열 targets의 열 길이입니다.
+int solution(int** targets, size_t targets_rows, size_t targets_cols) {
+    int answer = 0;
+    return answer;
+}
