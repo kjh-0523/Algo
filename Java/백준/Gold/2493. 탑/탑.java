5417ef46a744d82fbfdb8053bb4eed67247552f3
kjh-0523 <124140035+kjh-0523@users.noreply.github.com>
2025-07-23 14:17:00 +0900

    [Gold V] Title: 탑, Time: 2496 ms, Memory: 188960 KB -BaekjoonHub

---
+++ Java/백준/Gold/2493. 탑/탑.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static int N;
+    public static int[] tops;
+    public static int[] laser;
+    public static Stack<int[]> nums;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        tops = new int[N+1];
+        laser = new int[N+1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 1; i < N+1; i++) {
+            tops[i] = Integer.parseInt(st.nextToken());
+        }
+
+        nums = new Stack<>();
+        for(int i = N; i > 0; i--){
+            nums.push(new int[]{i, tops[i]});
+            if(tops[i-1] < nums.peek()[1]) continue;
+            while(!nums.isEmpty() && nums.peek()[1] <= tops[i-1]){
+                int[] cur = nums.pop();
+                laser[cur[0]] = i-1;
+            }
+        }
+
+        for(int i = 1; i < N+1; i++){
+            System.out.print(laser[i] + " ");
+        }
+
+    }
+}
