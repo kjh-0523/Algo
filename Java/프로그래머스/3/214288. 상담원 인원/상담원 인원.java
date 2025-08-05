4c6c8f9b37be379cdf3a85b79e22a912e9aad5d4
kjh-0523 <124140035+kjh-0523@users.noreply.github.com>
2025-08-05 11:48:10 +0900

    [level 3] Title: 상담원 인원, Time: 104.65 ms, Memory: 98.9 MB -BaekjoonHub

--- Java/프로그래머스/3/214288. 상담원 인원/상담원 인원.java
+++ Java/프로그래머스/3/214288. 상담원 인원/상담원 인원.java
@@ -1,75 +1,74 @@
 import java.util.*;
 
 class Solution {
-    public static int answer;
-    public static int k,n;
-    public static int[][] reqs;
+    public static int K, N, ans;
+    public static int[][] re;
+    
     public static class Participant implements Comparable<Participant>{
         int start, end;
         public Participant(int start, int end){
             this.start = start;
             this.end = end;
         }
-        @Override
-        public int compareTo(Participant o1){
-            // if(this.end == o1.end) return this.start - this.start;
-            return this.end - o1.end;
-        }
-    }
-    public int solution(int K, int N, int[][] Reqs) {
-        k = K;
-        n = N;
-        reqs = Reqs;
         
-        answer = Integer.MAX_VALUE;
-        dfs(n - k, 0, new int[k]);
-        return answer;
+        @Override
+        public int compareTo(Participant o){
+            return this.end - o.end;
+        }
+        
+    }
+    public int solution(int k, int n, int[][] reqs) {
+        K = k;
+        N = n;
+        re = reqs;
+        
+        ans = Integer.MAX_VALUE;
+        dfs(0, N - K, new int[K]);
+        return ans;
     }
     
-    public static void dfs(int remain, int idx, int[] cnt){
-        if(idx == k-1){
-            cnt[idx] = remain+1;
-            answer = Math.min(answer,couns(cnt));
-            // System.out.println(Arrays.toString(cnt) + answer);
+    public static void dfs(int idx, int remain, int[] cnt){
+        if(idx == K-1){
+            cnt[idx] = remain + 1;
+            ans = Math.min(ans, couns(cnt));
             return;
         }
         
-        for (int i = 0; i <= remain; i++) {
+        for(int i = 0; i < remain+1; i++){
             cnt[idx] = i+1;
-            dfs(remain - i, idx + 1, cnt);
+            dfs(idx + 1, remain - i, cnt);
         }
-        
     }
     
     public static int couns(int[] cnt){
-        PriorityQueue<Participant>[] q = new PriorityQueue[k];
-        for(int i = 0; i < k; i++){
-            q[i] = new PriorityQueue<>();
+        PriorityQueue<Participant>[] pq = new PriorityQueue[K];
+        for(int i = 0; i < K; i++){
+            pq[i] = new PriorityQueue<>();
         }
         
         int time = 0;
-        for(int[] req : reqs){
-            int s = req[0];
-            int e = s + req[1];
-            int t = req[2]-1;
+        for(int[] r : re){
+            int s = r[0];
+            int e = s + r[1];
+            int t = r[2] - 1;
             
-            if(q[t].isEmpty()){
-                q[t].offer(new Participant(s,e));
-                continue;
-            }
-            
-            if(q[t].size() >= cnt[t]){
-                Participant cur = q[t].poll();
-                if(cur.end > s){
-                    int remain = cur.end - s;
-                    time += remain;
-                    s += remain;
-                    e += remain;
-                    if(time >= answer) return time;
+            if(pq[t].isEmpty()){
+                pq[t].offer(new Participant(s, e));
+            }else{
+                if(pq[t].size() >= cnt[t]){
+                    Participant cur = pq[t].poll();
+                    if(cur.end > s){
+                        int wait = cur.end - s;
+                        time += wait;
+                        s += wait;
+                        e += wait;
+                        if(time >= ans) return time;
+                    }
                 }
+                pq[t].offer(new Participant(s,e));
             }
-            q[t].offer(new Participant(s,e));
         }
         return time;
     }
+    
 }
